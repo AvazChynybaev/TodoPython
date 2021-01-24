d6216e6b5e757f68867639049e90eb07b23d14df
avaz-web <zava23@gmail.com>
2021-01-24 23:54:24 +0600

    added book form in books.hmtl

--- main/views.py
+++ main/views.py
@@ -33,3 +33,17 @@ def add_todo(request):
     todo = ToDo(text=text)
     todo.save()
     return redirect(test)
+
+def add_book(request):
+    form = request.POST
+    book = Book(
+        title = form['title'],
+        subtitle = form['subtitle'],
+        description = form['description'],
+        price = form['price'],
+        genre = form['genre'],
+        author = form['author'],
+        year = form['year']
+    )
+    book.save()
+    return redirect(books)
