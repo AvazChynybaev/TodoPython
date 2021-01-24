19cccc87fd879dd55fd0627faf63d014a696b70b
avaz-web <zava23@gmail.com>
2021-01-24 23:11:44 +0600

    added todo form

--- todo/urls.py
+++ todo/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('added', added),
     path('changed', changed),
     path('deleted', deleted),
-    path('books/', books, name='books')
+    path('books/', books, name='books'),
+    path('add-todo/', add_todo, name='add-todo')
 ]   + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
