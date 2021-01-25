48e88153ae7dd6835eb2cf3051bb508d30e80bf7
avaz-web <zava23@gmail.com>
2021-01-25 10:24:31 +0600

    added todo marking and unmarking

--- main/views.py
+++ main/views.py
@@ -52,3 +52,10 @@ def delete_todo(request, id):
     todo = ToDo.objects.get(id=id)
     todo.delete()
     return redirect(test)
+
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = not todo.is_favorite
+    todo.save()
+    return redirect(test)
+
